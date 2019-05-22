3cd1fe44cc0dcc687b72d3e643e143e9bdef74a8
cooljmkill <coolj@DESKTOP-4Q5Q2SD>
2019-05-22 15:05:42 -0500

    added entities for user and address, as well as a class to test address

--- eDigital/src/main/java/com/techdigital/eDigital/Entities/User.java
+++ eDigital/src/main/java/com/techdigital/eDigital/Entities/User.java
@@ -1,5 +1,104 @@
 package com.techdigital.eDigital.Entities;
+import javax.persistence.*;
 
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	@ManyToOne
+    @JoinColumn(name = "addressId")
+	private Address addressId;
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+	
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the addressId
+	 */
+
+	public Address getAddressId() {
+		return addressId;
+	}
+	/**
+	 * @param addressId the addressId to set
+	 */
+	public void setAddressId(Address addressId) {
+		this.addressId = addressId;
+	}
 }
