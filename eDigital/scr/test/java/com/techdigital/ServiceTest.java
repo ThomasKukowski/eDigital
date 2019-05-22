5c63165a46d68f48ea13cb188df7d34f8ddc78a9
Thomas Kukowski <kukowskiThomas@gmail.com>
2019-05-22 12:30:21 -0500

    Second commit. Adds test folder and test class.

---
+++ eDigital/scr/test/java/com/techdigital/ServiceTest.java
@@ -0,0 +1,9 @@
+package com.techdigital;
+
+//When we need a more specific file, this can be changed.
+import static org.junit.*;
+
+//This class will be used to test our services. 
+public class ServiceTest {
+
+}
